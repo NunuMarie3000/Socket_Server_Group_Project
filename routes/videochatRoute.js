1adb665db6f00bfb8e218a6bc0b4b4f7ba9b1724
Storm O'Bryant <vmarie1997@gmail.com>
2022-09-20 12:06:51 -0500

    lunch break

--- routes/videochatRoute.js
+++ routes/videochatRoute.js
@@ -3,20 +3,33 @@ const router = express.Router()
 const http = require('http').createServer(router)
 const io = require('socket.io')(http, {
   cors: {
-    origin: ['http://localhost:3000']
+    origin: " * "
   }
 })
 
-io.on('connection', socket =>{
+io.on('connection', (socket) =>{
   console.log('Client connected')
+
+  // socket.on('join-room', (userId, roomId) => {
+  //   console.log(userId, roomId)
+  // })
+
+  socket.emit('join', socket.id)
+
   socket.on('disconnect', () => {
     console.log('user disconnect')
+    socket.broadcast.emit("call ended")
   })
 
-  socket.on('join-room', (userId, roomId) => {
-    console.log(userId, roomId)
+  socket.on("callUser", ({ userToCall, signalData, from, name})=>{    
+    io.to(userToCall).emit("callUser", {signal: signalData, from, name})
+  })
+
+  socket.on("answerCall", (data)=>{
+    io.to(data.to).emit("callAccepted", data.signal)
   })
 })
+
 io.listen(8080)
 
 router.get('/videochat',(req,res) => {
